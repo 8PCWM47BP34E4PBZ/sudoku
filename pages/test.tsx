ec9d3ad3305eadc7f60ce09c0898124864c79498
Yutaka Urakami <yutaka.urakami@avanade.com>
2022-04-07 16:13:22 +0900

    make design prototype

---
+++ pages/test.tsx
@@ -0,0 +1,280 @@
+import { Box } from "../components/box";
+import { themeContext, useDark } from "../contexts/theme";
+
+const Test = () => {
+  return (
+    <div className="container">
+      <div className="row justify-content-md-center">
+        <div
+          className="col col-lg-2"
+          style={{
+            color: "red",
+            backgroundColor: "blue",
+          }}
+          id="board-0"
+        >
+          Timer:
+        </div>
+        <div
+          className="col-md-auto"
+          style={{ color: "red", backgroundColor: "green" }}
+        ></div>
+        <div
+          className="col col-lg-2"
+          style={{ color: "red", backgroundColor: "blue" }}
+        ></div>
+        Action Btns
+      </div>
+      <div className="row">
+        <div className="col-md-6 offset-md-4">aaaaa</div>
+      </div>{" "}
+      <div className="row">
+        <div className="col-md-6 offset-md-4">
+          <div className="board-row">
+            <input
+              type={"number"}
+              className="square top-left-board-border"
+            ></input>
+            <input type={"number"} className="square top-board-border"></input>
+            <input
+              type={"number"}
+              className="square top-bord-right-block-border"
+            ></input>
+            <input
+              type={"number"}
+              className="square top-bord-left-block-border"
+            ></input>
+            <input type={"number"} className="square top-board-border"></input>
+            <input
+              type={"number"}
+              className="square square top-bord-right-block-border"
+            ></input>
+            <input
+              type={"number"}
+              className="square square top-bord-left-block-border"
+            ></input>
+            <input type={"number"} className="square top-board-border"></input>
+            <input
+              type={"number"}
+              className="square top-right-board-border"
+            ></input>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-6 offset-md-4">
+          <div className="board-row">
+            <input type={"number"} className="square left-board-border"></input>
+            <input type={"number"} className="square normal-border"></input>
+            <input type={"number"} className="square right-block-border"></input>
+            <input type={"number"} className="square left-block-border"></input>
+            <input type={"number"} className="square normal-border"></input>
+            <input type={"number"} className="square right-block-border"></input>
+            <input type={"number"} className="square left-block-border"></input>
+            <input type={"number"} className="square normal-border"></input>
+            <input type={"number"} className="square right-board-border"></input>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-6 offset-md-4">
+          <div className="board-row">
+            <input
+              type={"number"}
+              className="square left-bord-bottom-block-border"
+            ></input>
+            <input type={"number"} className="square bottom-block-border"></input>
+            <input
+              type={"number"}
+              className="square bottom-right-block-border"
+            ></input>
+            <input
+              type={"number"}
+              className="square bottom-left-block-border"
+            ></input>
+            <input
+              type={"number"}
+              className="square  bottom-block-border"
+            ></input>
+            <input
+              type={"number"}
+              className="square bottom-right-block-border"
+            ></input>
+            <input
+              type={"number"}
+              className="square bottom-left-block-border"
+            ></input>
+            <input type={"number"} className="square bottom-block-border"></input>
+            <input
+              type={"number"}
+              className="square right-board-bottom-block-border"
+            ></input>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-6 offset-md-4">
+          <div className="board-row">
+            <input
+              type={"number"}
+              className="square left-bord-top-block-border"
+            ></input>
+            <input type={"number"} className="square top-block-border"></input>
+            <input
+              type={"number"}
+              className="square top-right-block-border"
+            ></input>
+            <input
+              type={"number"}
+              className="square top-left-block-border"
+            ></input>
+            <input type={"number"} className="square top-block-border"></input>
+            <input
+              type={"number"}
+              className="square top-right-block-border"
+            ></input>
+            <input
+              type={"number"}
+              className="square top-left-block-border"
+            ></input>
+            <input type={"number"} className="square top-block-border"></input>
+            <input
+              type={"number"}
+              className="square right-board-top-block-border"
+            ></input>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-6 offset-md-4">
+          <div className="board-row">
+            <input type={"number"} className="square left-board-border"></input>
+            <input type={"number"} className="square normal-border"></input>
+            <input type={"number"} className="square right-block-border"></input>
+            <input type={"number"} className="square left-block-border"></input>
+            <input type={"number"} className="square normal-border"></input>
+            <input type={"number"} className="square right-block-border"></input>
+            <input type={"number"} className="square left-block-border"></input>
+            <input type={"number"} className="square normal-border"></input>
+            <input type={"number"} className="square right-board-border"></input>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-6 offset-md-4">
+          <div className="board-row">
+            <input
+              type={"number"}
+              className="square left-bord-bottom-block-border"
+            ></input>
+            <input type={"number"} className="square bottom-block-border"></input>
+            <input
+              type={"number"}
+              className="square bottom-right-block-border"
+            ></input>
+            <input
+              type={"number"}
+              className="square bottom-left-block-border"
+            ></input>
+            <input type={"number"} className="square bottom-block-border"></input>
+            <input
+              type={"number"}
+              className="square bottom-right-block-border"
+            ></input>
+            <input
+              type={"number"}
+              className="square bottom-left-block-border"
+            ></input>
+            <input type={"number"} className="square bottom-block-border"></input>
+            <input
+              type={"number"}
+              className="square right-board-bottom-block-border"
+            ></input>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-6 offset-md-4">
+          <div className="board-row">
+            <input
+              type={"number"}
+              className="square left-bord-top-block-border"
+            ></input>
+            <input type={"number"} className="square top-block-border"></input>
+            <input
+              type={"number"}
+              className="square top-right-block-border"
+            ></input>
+            <input
+              type={"number"}
+              className="square top-left-block-border"
+            ></input>
+            <input type={"number"} className="square top-block-border"></input>
+            <input
+              type={"number"}
+              className="square top-right-block-border"
+            ></input>
+            <input
+              type={"number"}
+              className="square top-left-block-border"
+            ></input>
+            <input type={"number"} className="square top-block-border"></input>
+            <input
+              type={"number"}
+              className="square right-board-top-block-border"
+            ></input>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-6 offset-md-4">
+          <div className="board-row">
+            <input type={"number"} className="square left-board-border"></input>
+            <input type={"number"} className="square normal-border"></input>
+            <input type={"number"} className="square right-block-border"></input>
+            <input type={"number"} className="square left-block-border"></input>
+            <input type={"number"} className="square normal-border"></input>
+            <input type={"number"} className="square right-block-border"></input>
+            <input type={"number"} className="square left-block-border"></input>
+            <input type={"number"} className="square normal-border"></input>
+            <input type={"number"} className="square right-board-border"></input>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-6 offset-md-4">
+          <div className="board-row">
+            <input
+              type={"number"}
+              className="square bottom-left-board-border"
+            ></input>
+            <input type={"number"} className="square bottom-board-border"></input>
+            <input
+              type={"number"}
+              className="square bottom-board-right-block-border"
+            ></input>
+            <input
+              type={"number"}
+              className="square bottom-board-left-block-border"
+            ></input>
+            <input type={"number"} className="square bottom-board-border"></input>
+            <input
+              type={"number"}
+              className="square bottom-board-right-block-border"
+            ></input>
+            <input
+              type={"number"}
+              className="square bottom-board-left-block-border"
+            ></input>
+            <input type={"number"} className="square bottom-board-border"></input>
+            <input
+              type={"number"}
+              className="square bottom-right-board-border"
+            ></input>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Test;
