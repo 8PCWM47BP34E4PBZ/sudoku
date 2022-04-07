96a7c5728c758450f5ae17598d226628091f416d
Yutaka Urakami <yutaka.urakami@avanade.com>
2022-04-07 17:38:38 +0900

    modify header

--- components/header.tsx
+++ components/header.tsx
@@ -4,15 +4,12 @@ import { Navbar, Nav } from "react-bootstrap";
 export default function Header() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="#home">
-        Sudoku
-      </Navbar.Brand>
+      <Navbar.Brand href="/">Sudoku</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/react-doc">React公式チュートリアル</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
